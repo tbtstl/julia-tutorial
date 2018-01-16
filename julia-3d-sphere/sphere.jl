863bad342eb078426a9ab2df8b058eeffce1fb93
Tyson Battistella <tysonbattistella@gmail.com>
2018-01-15 21:07:29 -0800

    update sphere.jl to use 2pi instead of pi

--- julia-3d-sphere/sphere.jl
+++ julia-3d-sphere/sphere.jl
@@ -1,12 +1,11 @@
 import PlotlyJS
 
-n = 3
+n = 50
 u = linspace(0, 2pi, n)
-v = linspace(0, pi, n)
-
+v = linspace(0, 2pi, n)
 r = 1
-x = cos(u) * sin(v)'
-y = sin(u) * sin(v)'
-z = repeat(cos(v)', outer=[n, 1])
+x = r * cos(u) * sin(v)'
+y = r * sin(u) * sin(v)'
+z = repeat(r * cos(v)', outer=[n, 1])
 
 PlotlyJS.plot([PlotlyJS.surface(x=x, y=y, z=z)])
